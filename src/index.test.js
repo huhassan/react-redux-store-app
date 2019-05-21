1b56bb2602fc7fd416570e02bd4d2f76b232f2c4
Hussam Hassan <hussam@tuta.io>
2019-05-21 17:23:06 +0200

    confirming user.email update

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,16 @@
-it('should work', function() {
-    expect(true).toEqual(true);
-});
+import { action, reducer } from '';
+
+describe('Counter', function () {
+    it('should have an initial state', function () {
+        const result = reducer();
+        expect(result.count).toEqual(0);
+    })
+})
+
+describe('Counter', function () {
+    it('should increase by one', function () {
+        const result = reducer();
+        const action = actionCreator()
+        expect(result.count).toEqual(1);
+    })
+})
